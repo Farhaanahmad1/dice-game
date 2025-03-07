69dfb8c47b40e019a7915067c279d50ca0ae69d0
Farhan ahmad <farhanahmid1@gmail.com>
2025-03-07 15:21:17 +0500

    done

--- script.js
+++ script.js
@@ -22,10 +22,11 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 
 // rolling dice functionality
-
+if(playing){
 document.querySelector('.btn--roll').addEventListener('click', function(){
     // 1. generate a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -47,4 +48,49 @@ document.querySelector('.btn--roll').addEventListener('click', function(){
         player0El.classList.toggle('player--active');
         player1El.classList.toggle('player--active');
     }
+}
+)}
+
+
+btnHold.addEventListener('click', function(){
+    // add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+
+    // check score atleast 100
+    if (scores[activePlayer] >= 100){
+        // finish the game
+        playing = false;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    } else {
+        // switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+    }
 })
+
+
+
+// RESET THE GAME
+btnNew.addEventListener('click', function(){
+    scores[0] = 0;
+    scores[1] = 0;
+    currentScore = 0;
+    activePlayer = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+)
+
+
